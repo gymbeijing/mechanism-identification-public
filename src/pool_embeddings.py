2e743533acfc98293881a86d2d5519f237a39c96
Ubuntu <ubuntu@ip-10-91-182-40.us-west-2.compute.internal>
2023-06-22 13:42:57 +0000

    Updated pool_embeddings.py

--- src/pool_embeddings.py
+++ src/pool_embeddings.py
@@ -12,7 +12,7 @@ def load_metadata(metadata_filepaths):
 		with open(metadata_filepath, 'r') as f:
 			d = json.load(f)
 			metadata_dict[vid] = ['_'.join(name.split('_')[:-1]) for name in d]
-		print(f'{vid}: {len(metadata_dict[vid])}')
+		#print(f'{vid}: {len(metadata_dict[vid])}')
 
 	return metadata_dict
 
@@ -20,7 +20,7 @@ def load_metadata(metadata_filepaths):
 def compute_intersect(metadata_dict):
 	metadata_list = list(metadata_dict.values())
 	intersect = set.intersection(*map(set, metadata_list))
-	print(f'length of intersection: {len(intersect)}')
+	#print(f'length of intersection: {len(intersect)}')
 	return intersect
 
 
@@ -30,17 +30,54 @@ def load_emb(emb_filepaths):
 		vid = emb_filepath.split('/')[-1].split('.')[0].split('_')[-1]
 		with open(emb_filepath, 'r') as f:
 			emb_dict[vid] = torch.load(emb_filepath)
-		print(f'{vid}: {emb_dict[vid].shape}')
+		#print(f'{vid}: {emb_dict[vid].shape}')
 	
 	return emb_dict
+
+
+def filter_emb(emb_dict, metadata_dict):
+	emb_filtered_dict = dict()
+	for vid, emb in emb_dict.items():
+		m_data = metadata_dict[vid]
+		keep_indice = []
+		for idx, d in enumerate(m_data):
+			if d in intersect:
+				keep_indice.append(idx)
+		emb_dict[vid] = emb[keep_indice]
+		#print(f'{vid}: {emb_dict[vid].shape}')   # [141245, 512]
+		if vid == '00':
+			new_metadata = [metadata_dict[vid][idx] for idx in keep_indice]
+
+	return emb_dict, new_metadata
 		
+
+def pool_embedding(emb_dict, strategy='mean'):
+	embs = list(emb_dict.values())
+	stacked_emb = torch.stack(embs, 0)   # [24, 141245, 512]
+
+	if strategy == 'mean':
+		pooled_emb = torch.mean(stacked_emb, dim=0)
+	if strategy == 'max':
+		pooled_emb = torch.max(stacked_emb, dim=0).values
+
+	return pooled_emb
+
+
+def save_emb_to_file(emb, metadata, args):
+	print(f'Saving {args.pooling_strategy} pooled embeddings to {args.embs_folder}/{args.pooling_strategy}_pooled_emb.pt')
+	torch.save(emb, f'{args.embs_folder}/{args.pooling_strategy}_pooled_emb.pt')
+	print(f'Saving filtered embedding metadata to {args.embs_folder}/emb_idx_filtered.json')
+	with open(f'{args.embs_folder}/emb_idx_filtered.json', 'w') as fw:
+		json.dump(metadata, fw, indent=4, ensure_ascii=False, sort_keys=False)
+
+	return
 
 
 if __name__ == '__main__':
 	# Parse the arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--embs_folder', type=str, help='Path to the folder that saves all the embeddings')
-	parser.add_argument('--pooling_strategy', type=str, help='Pooling strategy to be used')
+	parser.add_argument('--embs_folder', type=str, default='../emb', help='Path to the folder that stores all the embeddings')
+	parser.add_argument('--pooling_strategy', type=str, default='mean', help='Pooling strategy to be used')
 	args = parser.parse_args()
 
 	# Load in all the image embeddings
@@ -55,4 +92,13 @@ if __name__ == '__main__':
 	# Compute the intersect parts name
 	intersect = compute_intersect(metadata_dict)   # 141245
 	
+	# Load embedding into a dictionary
 	emb_dict = load_emb(emb_filepaths)
+
+	# Filter out the part that doesn't have 24 views
+	emb_dict, new_metadata = filter_emb(emb_dict, metadata_dict)
+
+	# Pool embeddings according to the pooling strategy
+	pooled_emb = pool_embedding(emb_dict, args.pooling_strategy)   # [141245, 512]
+
+	save_emb_to_file(pooled_emb, new_metadata, args)
