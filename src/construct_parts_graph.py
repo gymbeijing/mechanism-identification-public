8cd162c05c09ae8130e7cb1bde797917157d4568
Ubuntu <ubuntu@ip-10-91-182-45.us-west-2.compute.internal>
2023-06-27 16:56:43 +0000

    Completed construct_parts_graph.py

--- src/construct_parts_graph.py
+++ src/construct_parts_graph.py
@@ -3,6 +3,7 @@ import json
 import logging
 import argparse
 import glob
+from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
@@ -18,6 +19,7 @@ def read_json(file_path):
 	
 
 def build_graph(info):
+
 	G = nx.Graph()
 	bodies = info["bodies"]
 	G.add_nodes_from(bodies)
@@ -34,14 +36,15 @@ def build_graph(info):
 
 			return G
 		else:
-			logging.info(f'contacts is null')
+			#logging.info(f'contacts is null')
 			return None
 	else:
-		logging.info(f'not having contacts in info.keys()')
+		#logging.info(f'not having contacts in info.keys()')
 		return None
 
 
-def get_graphs(assembly_ids, assembly_folder):
+def compute_graphs(assembly_ids, assembly_folder):
+
 	graph_dict = dict()
 	for a_id in assembly_ids:
 		#print(a_id)
@@ -51,11 +54,51 @@ def get_graphs(assembly_ids, assembly_folder):
 		#print(a_graph.edges)
 		if a_graph is not None:
 			graph_dict[a_id] = a_graph
-		else:
-			logging.info(f'{a_id}')
+		#else:
+		#	logging.info(f'{a_id}')
 	
 	return graph_dict
+
+
+def order_parts(neighbours, assembly_parts_to_order_map):
+
+	neighbours_to_order_map = {part: assembly_parts_to_order_map[part] for part in neighbours if part in assembly_parts_to_order_map}
+	sorted_neighbours_order_tuple_list = sorted(neighbours_to_order_map.items(), key=lambda x: x[1]) 
+	ordered_neighbours = [item[0] for item in sorted_neighbours_order_tuple_list]
+
+	return ordered_neighbours
+
+
+def compute_part_graphs(graph_dict, all_parts_order):
 	
+	part_graph_dict = dict()
+	for a_id, a_graph in tqdm(graph_dict.items()):
+		a_nodes = a_graph.nodes
+		part_graph_dict[a_id] = dict()   # warning: didn't perform further check, might include assembly having empty part graph
+		a_parts_order = all_parts_order[a_id]   # list
+
+		a_parts_to_order_map = dict()
+		for idx, part in enumerate(a_parts_order):
+			a_parts_to_order_map[part] = idx
+
+		for a_node in a_nodes:
+			#print(a_node)
+			a_neighbours = a_graph.neighbors(a_node)
+			a_neighbours = [f'0000_{a_id}_{a_n}_0000'for a_n in a_neighbours]
+			if len(a_neighbours) != 0:
+				ordered_a_neighbours = order_parts(a_neighbours, a_parts_to_order_map)
+				if len(ordered_a_neighbours) != 0:
+					part_graph_dict[a_id][f'0000_{a_id}_{a_node}_0000'] = ordered_a_neighbours
+	
+	return part_graph_dict
+
+
+def write_json(content, filepath):
+
+	with open(filepath, 'w', encoding='utf8') as fp:
+		json.dump(content, fp, indent=4, ensure_ascii=False, sort_keys=False)
+	
+	return
 
 
 if __name__ == '__main__':
@@ -63,6 +106,8 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--assembly_id_path', type=str, help='Path to the file that stores all the processed assembly ids')
 	parser.add_argument('--assembly_folder', type=str, help='Path to the folder that stores all the assembly.json files')
+	parser.add_argument('--assembly_parts_order', type=str, help='Path to the file that stores the mapping from assembly id and computed orders of its parts')
+	parser.add_argument('--save_path', type=str, help='Path to the file that will save the part graphs')
 	args = parser.parse_args()
 
 	# Load in the assembly ids from the view folders into a list
@@ -75,6 +120,20 @@ if __name__ == '__main__':
 	assembly_json_list = glob.glob(f'{args.assembly_folder}/*.json')
 	logging.info(f'Found {len(assembly_json_list)} assembly json files in {args.assembly_folder}')
 
-	#print(assembly_json_list[:10])
-	assembly_graph_dict = get_graphs(assembly_id_list, args.assembly_folder)
-	print(len(assembly_graph_dict.keys()))
+	# Compute whole graph for each assembly
+	assembly_graph_dict = compute_graphs(assembly_id_list, args.assembly_folder)
+	logging.info(f'Found {len(assembly_graph_dict.keys())} assembly json files that have proper contacts information')
+
+	assembly_parts_order = read_json(args.assembly_parts_order)
+	logging.info(f'Computing part graphs')
+	part_graph_dict = compute_part_graphs(assembly_graph_dict, assembly_parts_order)
+
+	logging.info(f'Saving part graphs into {args.save_path}')
+	write_json(part_graph_dict, args.save_path)
+
+	total = 0
+	for p_graphs in part_graph_dict.values():
+		total += len(p_graphs.keys())
+
+	logging.info(f'Found {total} part graphs.')
+
