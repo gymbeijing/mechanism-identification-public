aa2586987f6cafcf0ae6dbe78f2b52f2189c6af8
Ubuntu <ubuntu@ip-10-91-182-212.us-west-2.compute.internal>
2023-07-05 14:42:36 +0000

    Completed the Dataset class

--- data/ae_dataset.py
+++ data/ae_dataset.py
@@ -2,28 +2,46 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import json
 import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+
+
+def get_dataloader(phase, config, shuffle=None):
+	is_shuffle = phase=='train' if shuffle is None else shuffle
+
+	dataset = AEDataset()
+	dataloader = DataLoader(dataset, batch_size=config.batch_size, shuffle=is_shuffle)
+	return dataloader
+	
 
 
 class AEDataset(Dataset):
-	def __init__(self, config):
+	#def __init__(self, config):
+	def __init__(self):
 		super(AEDataset, self).__init__()
 	
 		# All the embeddings
-		all_emb_path = os.path.join(config.emb_dir, "mean_pooled_emb.pt")
+		#all_emb_path = os.path.join(config.emb_dir, "mean_pooled_emb.pt")
+		all_emb_path = os.path.join("../processed_data", "mean_pooled_emb.pt")
 		self.all_emb = torch.load(all_emb_path)   # [141245, 512]
 		pad = torch.zeros(1, 512)
 		self.all_emb = torch.cat((self.all_emb, pad), 0) # [141246, 512]
 		
 		# All the metadata
-		all_md_path = os.path.join(config.emb_dir, "emb_idx_filtered.json")
+		#all_md_path = os.path.join(config.emb_dir, "emb_idx_filtered.json")
+		all_md_path = os.path.join("../processed_data", "emb_idx_filtered.json")
 		with open(all_md_path, 'r') as fp:
 			self.all_md = json.load(fp)   # 141245
-		self.all_md["pad"] = self.all_emb.shape[0]-1   # 141246
+		self.all_md.append("pad")   # 141246
 
-		n_neighbour = config.n_neighbour
+		#n_neighbour = config.n_neighbour
+		n_neighbour = 10
 
 		# All the preprocessed part graphs
-		all_part_graph_path = os.path.join(config.emb_dir, "part_graphs_dim=5.json")
+		#all_part_graph_path = os.path.join(config.emb_dir, "part_graphs_dim=5.json")
+		all_part_graph_path = os.path.join("../processed_data", "part_graphs_dim=5.json")
 		with open(all_part_graph_path, 'r') as fp:
 			self.all_part_graph = json.load(fp)
 		
@@ -33,27 +51,41 @@ class AEDataset(Dataset):
 			n_part_graph += len(a_graphs)
 		
 		# Map part name to its idx in the metadata/embeddings
-		self.prt_name_idx_map = dict()
+		part_name_idx_map = dict()
 		for idx, p_name in enumerate(self.all_md):
-			prt_name_idx_map[p_name] = idx
+			part_name_idx_map[p_name] = idx
 
-		self.all_data = torch.ones(n_part_graph, n_neighbour+1)*self.prt_name_idx["pad"]   # initialize to be all pad's index 
+		logging.info(f'pad\'s corresponding index is {part_name_idx_map["pad"]}')
 
+		self.all_data = torch.ones(n_part_graph, n_neighbour+1)*part_name_idx_map["pad"]   # initialize to be all pad's index 
+
+		# Fill in the matrix
 		r = 0
 		for a_id, a_graphs in self.all_part_graph.items():
 			for c_part, neigh_parts in a_graphs.items():
-				self.all_data[r][0] = self.prt_name_idx_map[c_part]
+				if r == 0:
+					print(c_part)
+				self.all_data[r][0] = part_name_idx_map[c_part]
 				c = 1
 				for neigh_part in neigh_parts:
+					if r == 0:
+						print(neigh_part)
 					if c <= n_neighbour:   # only keep the first n_neighbour neighbouring parts
-						self.all_data[r][c] = self.prt_name_idx_map[neigh_part]
+						self.all_data[r][c] = part_name_idx_map[neigh_part]
 					else:
 						break
 					c += 1
-			r += 1
+				r += 1
 
 	def __len__(self):
 		return self.all_data.shape[0]
 
 	def __getitem__(self, idx):
 		return self.all_data[idx]   # [512]
+
+
+if __name__ == "__main__":
+	dataset = AEDataset()
+	print(dataset[0])
+	print(dataset[1])
+	print(len(dataset))
