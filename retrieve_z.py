95da71df8bba4a004e5716d569b1d6c932637468
Ubuntu <ubuntu@ip-10-91-182-212.us-west-2.compute.internal>
2023-07-11 09:52:06 +0000

    Saved z

--- retrieve_z.py
+++ retrieve_z.py
@@ -3,6 +3,11 @@ from model.autoencoder import AutoEncoder
 from config.configAE import ConfigAE
 from data.ae_dataset import get_dataloader
 
+
+def save_tensor(t, dest):
+    torch.save(t, dest)
+
+
 if __name__ == '__main__':
     ckpt_file = 'lightning_logs/0711/083732/checkpoints/best.ckpt'
     cfg = ConfigAE('train')
@@ -16,4 +21,7 @@ if __name__ == '__main__':
         batch_z.append(z)
 
     all_z = torch.cat(batch_z, dim=0)
-    print(all_z.shape)
+    # print(all_z.shape)
+    path = "model_outputs/z_train.pt"
+    save_tensor(all_z, path)
+
